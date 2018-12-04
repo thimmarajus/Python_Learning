ccd88dc91e2067a396c9d41d20f258bb1702c517
thimmaraju <thimmarajus@gmail.com>
2018-12-04 10:26:22 +0800

    first.py

--- first.py
+++ first.py
@@ -1,1 +1,71 @@
-print"Hello Python WOrld"
+"""This is a
+             multiline docstring."""
+#print"Hello Python WOrld"
+"""if 5 > 2:
+ print("Five is greater than two!")
+else:
+ print("Five is lesser than two!")"""
+"""x = 5
+y = "John"
+print(x)
+print(y)"""
+"""x = 4 # x is of type int
+x = "Sally" #x is now of type str
+print(x)"""
+"""x = "awesome"
+print("Python is " +x)"""
+"""x = "Python is "
+y = "awsome"
+z = x + y
+print(z)"""
+"""x = 5
+y = 10
+print(x + y)"""
+#x = 5
+#y = "John"
+#print(x + y)
+"""x = 1 # int
+y = 2.8 # float
+z = 1j # complex
+print(type(x))
+print(type(y))
+print(type(z))"""
+"""x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))"""
+"""x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))"""
+"""x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))"""
+"""x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))"""
+"""a = "Hello, World!"
+print(a[2])"""
+"""b = "Hello, World!"
+print(b[2:5])"""
+a = "Hello, World!"
+#print(a.strip())
+#print(a)
+#print(len(a))
+#print(a.lower())
+#print(a.upper())
+#print(a.replace("H","J"))
+a = "Hello, World!"
+#print(a.split(","))
+print(a)
