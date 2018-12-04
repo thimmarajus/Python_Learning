02b4633c0a2f46fca832291023b5ee5ca315b2a2
thimmaraju <thimmarajus@gmail.com>
2018-12-04 14:28:08 +0800

    second.py

---
+++ second.py
@@ -0,0 +1,76 @@
+"""print("Enter your name:")
+x = input()
+print("Hello, " + x)"""
+"""x = 5
+y = 3
+print(x / y)"""
+"""thislist = ["apple", "banana", "cherry"]
+print(thislist)"""
+"""thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)"""
+"""thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+ print(x)"""
+"""thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+ print("yes, 'apple' is in the fruits list")"""
+#thislist = ["apple", "banana", "cherry"]
+""""thislist.append("orange")
+#print(thislist)"""
+#thislist.remove("banana")
+#print(thislist)
+#thislist.pop()
+#print(thislist)
+#thislist = (("apple", "banana", "cherry"))
+#del thislist[0]
+#thislist.clear()
+#print(thislist)
+#print(thislist)
+#thistuple = ("apple", "banana", "cherry")
+#thistuple[1] = "blackcurrant"
+#print(thistuple)
+#print(thistuple[1])
+#thistuple = ("apple", "banana", "cherry")
+#for x in thistuple:
+# print(x)
+"""thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+ print("yes, 'apple' is in the fruits tuple")"""
+#thistuple = ("apple", "banana", "cherry")
+#if "apple" in thistuple:
+ #print("yes, 'apple' is in the fruits tuple")
+#else:
+ #print("No, 'apple' is not in the fruits tuple")
+#print(len(thistuple))
+#thistuple[3] = "Orange"
+#print(thistuple)
+#del thistuple
+#print(thistuple)
+#thistuple = tuple(("appple", "banana", "cherry"))
+#print(thistuple)
+#thisset = {"apple", "banana", "cherry"}
+#del thisset
+#thisset.clear()
+#print(thisset)
+#thisset.discard("banana8")
+#x = thisset.pop()
+#print(x)
+#print(thisset)
+#for x in thisset:
+#print(x)
+#print("banana" in thisset)
+#thisset.add("Orange")
+#print(thisset)
+#thisset.update(["orange","Mango","Grapes"])
+#print(thisset)
+#thisset.remove("banana")
+#print(len(thisset))
+#thisset = set(("apple", "banana", "cherry"))
+x = {"apple","banana","cherry"}
+y = {"google","microsoft","apple"}
+x.difference_update(y)
+print(x)
+#x = thisset.copy()
+#print(x)
+#print(thisset)
