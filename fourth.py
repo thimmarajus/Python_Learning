86e71c1461b85f02ff89b75726ee24f6ab99e7b2
thimmaraju <thimmarajus@gmail.com>
2018-12-04 17:36:38 +0800

    fourth.py

---
+++ fourth.py
@@ -0,0 +1,39 @@
+def my_function(x):
+ return 5 * x
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+#def my_function(country  = "Norway"):
+#    print("I am from\t" + country)
+#my_function()
+#my_function("Sweden")
+#my_function("India")
+#my_function("brazil")
+#def my_function(fname):
+ #print(fname + "\t Refsnes")
+#my_function("Emil")
+#my_function("Tobias")
+#my_function("Linus")
+#def my_function():
+# print("Hello from a function")
+#my_function()
+#i = 0
+#while i < 6:
+ #i += 1 
+#print(i)
+ #if i == 3:
+  #continue
+ #print(i)
+# i += 1
+#i = 1
+#while i < 6:
+ #print(i)
+ #i += 1
+#print(i)
+#a = 70
+#b = 30
+#c = 90
+#print("a") if a > b else print("b")
+#if a > b: print("a is greater than b")
+#if a > b and c > a:
+ #print("Both condtions are True")
